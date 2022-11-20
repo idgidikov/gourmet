0f3e84a7dc1746f0bc43cc67b89cb988f6530940
Evgeni Petrov <evgenipetrov@Evgenis-MBP.lan>
2022-11-20 17:39:03 +0200

    Eslint and Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	return (
+		<div className="App">
+			<h1 className="main-header">Under production</h1>
+			<p>
+				This website will be part of portfolio under construction of Ivan,
+				Plamen and Evgeni
+			</p>
+			<p>Every feature will be implemented by the following roles</p>
+			<ul>
+				<li className="">Using React</li>
+				<li className="">Well maintain libraries</li>
+				<li className="">Eslint configuration</li>
+				<li className="">CI/CD</li>
+				<li className="">Deploy to Firebase</li>
+			</ul>
+			<h3>This is a test</h3>
+		</div>
+	);
 }
 
-export default App
+export default App;
