a972dd549535f9515f0339406bbdd0ee4358b6af
Evgeni Petrov <evgenipetrov@Evgenis-MBP.lan>
2022-11-20 17:43:35 +0200

    Eslint and Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
 	const [count, setCount] = useState(0);
