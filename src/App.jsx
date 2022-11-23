9c75320951ba59376b14366a56204c7799616689
idgidikov <idgidikov@abv.bg>
2022-11-23 13:26:06 +0200

    feat: install tailwindcss and daisy ui #16
    to install tailwindcss https://tailwindcss.com/docs/guides/vite
    to install https://daisyui.com/docs/install/

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,12 @@ function App() {
 	return (
 		<div className="App">
 			<h1 className="main-header">Under production</h1>
-			<p>
-				This website will be part of portfolio under 
-				construction of Ivan,
-				Plamen and Evgeni.
-			</p>
 			<p>Every feature will be implemented by the following roles</p>
 			<ul>
-				<li className="">Using React</li>
-				<li className="">Well maintain libraries</li>
+				<li className="">
+					<h1 className="text-red-400">Using React</h1>
+				</li>
+				<li className="btn btn-primary">Well maintain libraries</li>
 				<li className="">Eslint configuration</li>
 				<li className="">CI/CD</li>
 				<li className="">Deploy to Firebase</li>
